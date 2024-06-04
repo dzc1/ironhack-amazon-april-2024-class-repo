952d9a954fab0d20e1ca5a369492198c4a872390
Diego Zito <diegozito.m@gmail.com>
2024-06-04 21:11:10 +0200

    update - week 9 - comps added

--- weeks/weeks-9-10-11/my-first-vue-project/src/components/exercises/02-Vfor.vue
+++ weeks/weeks-9-10-11/my-first-vue-project/src/components/exercises/02-Vfor.vue
@@ -10,3 +10,15 @@ favoritePizzaType - String
 favoriteDrinks -Array (multiple)
 favoriteBurgertypes - Array (multiples)
 Create a component andd. store this info aduse the v-for directive to loop through and display theinfo withinn the template -->
+
+<!-- HTML -->
+<template></template>
+
+<!-- JS  -->
+<script setup>
+import { ref, reactive, computed } from "vue";
+</script>
+
+<!-- CSS -->
+<!-- The scoped attribute means that only the CSS properties available inside this style tag will be applied inside this component and ONLY This component -->
+<style scoped></style>
