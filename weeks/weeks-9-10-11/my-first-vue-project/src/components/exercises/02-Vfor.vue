c0d6a3e3ac0dac9f5133c80cd2fdf8c695d68d36
Diego Zito <diegozito.m@gmail.com>
2024-06-04 21:10:38 +0200

    update - week 9 - comps added

---
+++ weeks/weeks-9-10-11/my-first-vue-project/src/components/exercises/02-Vfor.vue
@@ -0,0 +1,12 @@
+<!-- iteration exercise:
+
+Create a reactive object that contains information:
+
+String - Name
+Number- Age
+Array - Favorite Music Bands
+Object - 3 Keys
+favoritePizzaType - String
+favoriteDrinks -Array (multiple)
+favoriteBurgertypes - Array (multiples)
+Create a component andd. store this info aduse the v-for directive to loop through and display theinfo withinn the template -->
